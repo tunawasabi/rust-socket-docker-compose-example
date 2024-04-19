db20ed9a02e790d9b3110577713e233ccbc17de2
tunawasabi <77270077+tunawasabi@users.noreply.github.com>
2024-04-20 00:32:26 +0900

    Create `receiver` crate

--- reciver/src/main.rs
+++ reciver/src/main.rs
@@ -1,3 +1,31 @@
+use std::{
+    io::Read,
+    net::{TcpListener, TcpStream},
+};
+
+use env;
+
 fn main() {
-    println!("Hello, world!");
+    let host = env::get_ip_address();
+    let port = env::get_default_port();
+
+    let Ok(listner) = TcpListener::bind(format!("{}:{}", host, port)) else {
+        panic!("Could not connect to server or bind to port.");
+    };
+
+    for stream in listner.incoming() {
+        let Ok(stream) = stream else {
+            break;
+        };
+        handle(stream);
+    }
+
+    println!("Connection closed.")
+}
+
+fn handle(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).ok();
+
+    println!("Recieve: {}", String::from_utf8_lossy(&buffer[..]));
 }
--- sender/src/main.rs
+++ sender/src/main.rs
@@ -1,3 +1,15 @@
+use env;
+use std::{io::Write, net::TcpStream, thread, time::Duration};
+
 fn main() {
-    println!("Hello, world!");
+    let host = env::get_ip_address();
+    let port = env::get_default_port();
+
+    let Ok(mut stream) = TcpStream::connect(format!("{}:{}", host, port)) else {
+        panic!("Could not connect to the server or the server is not running");
+    };
+
+    while let Ok(_) = stream.write(b"Hello, World!") {
+        thread::sleep(Duration::from_secs(1));
+    }
 }
